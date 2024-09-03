79029dc7761ca81e7ff856d7935b35ed7504106c
Robert Balaban <rob.balaban@gmail.com>
2024-09-04 01:07:38 +0300

    testing(nix): debugging

--- flake.nix
+++ flake.nix
@@ -1,7 +1,13 @@
 {
   description = "Frosted flakes, just add milk!";
 
-  outputs = { self, nixpkgs }: {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";  # adjust for your system
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
     templates = {
       rust = {
         path = ./rust;
@@ -15,6 +21,22 @@
         path = ./java;
         description = "A Java template";
       };
+    };
+
+    apps.${system}.init = {
+      type = "app";
+      program = pkgs.writeShellScriptBin "init-flake" ''
+        if [ $# -ne 2 ]; then
+          echo "Usage: init-flake <template> <project-name>"
+          exit 1
+        fi
+        template=$1
+        project_name=$2
+        mkdir -p "$project_name" && \
+        cd "$project_name" && \
+        ${pkgs.nix}/bin/nix flake init -t "github:robalaban/frosted-flakes#$template" && \
+        ${pkgs.direnv}/bin/direnv allow
+      ''.outPath;
     };
   };
 }
