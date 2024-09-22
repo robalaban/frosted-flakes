c90cd21bba111c0d1b7f5bd660a7d66368274da0
Robert Balaban <rob.balaban@gmail.com>
2024-09-22 14:58:58 +0300

    feat(python): flakes

--- python/flake.nix
+++ python/flake.nix
@@ -2,27 +2,34 @@
   description = "A Python project using uv as the package manager";
 
   inputs = {
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system =  "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-      pythonEnv = pkgs.python312.withPackages (ps: with ps; [
-        pip
-        virtualenv
-        poetry
-        numpy
-        pandas
-      ]);
-    in
-    {
-      defaultPackage.${system} = pythonEnv;
-      devShell.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pythonEnv
-        ];
-      };
+  outputs = { self, nixpkgs, flake-parts }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-darwin" ];
+
+      perSystem = { system, pkgs, ... }:
+        {
+          packages = {
+            default = pkgs.python312.withPackages (ps: with ps; [
+              pip
+              virtualenv
+              poetry
+              numpy
+              pandas
+            ]);
+          };
+          devShells = {
+            default = pkgs.mkShell {
+              packages = with pkgs; [ python312 ];
+            };
+          };
+        };
     };
 }
